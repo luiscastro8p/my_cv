adddaf98234a91e5f1822734c1923191df10b0da
Luis Castro <luisarmandocastro12@gmail.com>
2023-08-23 16:33:32 -0700

    add image and buttons in Me

--- src/views/home/me/index.jsx
+++ src/views/home/me/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./index.css";
 import TypingEffect from "../../../components/typeWriteEffect";
+import github from "../../../assets/img/github.png";
+
+import facebook from "../../../assets/img/facebook.png";
+import instagram from "../../../assets/img/instagram.png";
+import download from "../../../assets/img/download.png";
 
 const Me = () => {
   return (
@@ -8,10 +13,49 @@ const Me = () => {
       <div className="home-body">
         <div className="content">
           <h2>Hola, soy Luis Castro</h2>
-          <TypingEffect data={["Desarrollador web","Ing. informático", "Técnico en computación"]} />
-          <small>
-            Lorem ipsum dolor sit amet consectetur adipisicing.
-          </small>
+          <TypingEffect
+            data={[
+              "Desarrollador web",
+              "Ing. informático",
+              "Técnico en computación",
+            ]}
+          />
+          <small>Lorem ipsum dolor sit amet consectetur adipisicing.</small>
+          <div className="buttons-body">
+            <button className="btn-dark">Más sobre mis</button>
+            <button className="btn-white">
+              Resumen
+              <img
+                src={download}
+                alt="download"
+                width={13}
+                style={{ marginLeft: "8px" }}
+              />
+            </button>
+          </div>
+          <div className="img-body">
+            <a
+              href="https://github.com/luiscastro8p"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={github} alt="github" />
+            </a>
+            <a
+              href="https://www.facebook.com/luiscastro8p/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={facebook} alt="facebook" />
+            </a>
+            <a
+              href="https://www.instagram.com/luiscastro8p"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instagram} alt="instagram" />
+            </a>
+          </div>
         </div>
       </div>
       <div style={{ position: "relative" }}>
@@ -19,7 +63,6 @@ const Me = () => {
           <div className="small-point" />
           <div className="big-point" />
         </div>
-        {/*     <img src={perfil} alt="MrLACC" className="profile" /> */}
       </div>
     </section>
   );
